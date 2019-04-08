cfebc9d268518202110bef98d543561c895c3df2
Paola <paola@MacBook-Air-de-Omnibnk.local>
2019-04-08 09:26:55 -0500

    Adding the ingredient feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -55,3 +55,12 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredients_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Test ingredient'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
---
+++ app/recipe/tests/test_ingredients_api.py
@@ -0,0 +1,65 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+
+class PublicIngredientsAPITest(TestCase):
+    """Testing the publicly available ingredients API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Tests that the login is required to access the API"""
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsAPITest(TestCase):
+    """Testing the ingredients authorized API"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'test@admin.com',
+            'password123'
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredients_list(self):
+        """Test listing the ingredients"""
+        Ingredient.objects.create(user=self.user, name='Sugar')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        """Test listing the ingredients are listing for the authorized user"""
+        user2 = get_user_model().objects.create_user(
+            'test1@admin.com',
+            'password123'
+        )
+
+        ingredient = Ingredient.objects.create(user=self.user, name='Salt')
+        Ingredient.objects.create(user=user2, name='Sugar')
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
